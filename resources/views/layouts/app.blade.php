fcbc460ec54d0fc2fc1b58ea2735b98c65140148
fazza7777 <aungbarlaay7777@gmail.com>
2022-09-24 02:23:57 +0630

    setup home page and side bar ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,65 +26,127 @@
 </head>
 
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <div class="page-wrapper chiller-theme">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+        <nav id="sidebar" class="sidebar-wrapper">
+            <div class="sidebar-content">
+                <div class="sidebar-brand">
+                    <a href="#">Office</a>
+                    <div id="close-sidebar">
+                        <i class="fas fa-times"></i>
+                    </div>
+                </div>
+                <div class="sidebar-header">
+                    <div class="user-pic">
+                        <img class="img-responsive img-rounded"
+                            src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+                            alt="User picture">
+                    </div>
+                    <div class="user-info">
+                        <span class="user-name">Jhon
+                            <strong>Smith</strong>
+                        </span>
+                        <span class="user-role">Administrator</span>
+                        <span class="user-status">
+                            <i class="fa fa-circle"></i>
+                            <span>Online</span>
+                        </span>
+                    </div>
+                </div>
+                <!-- sidebar-header  -->
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+                <div class="sidebar-menu">
+                    <ul>
+                        <li class="header-menu">
+                            <span>General</span>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-home"></i>
+                                <span>Home</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-users"></i>
+                                <span>Employees</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-dropdown">
+                            <a href="#">
+                                <i class="fa fa-globe"></i>
+                                <span>Maps</span>
+                            </a>
+                            <div class="sidebar-submenu">
+                                <ul>
+                                    <li>
+                                        <a href="#">Google maps</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Open street map</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="header-menu">
+                            <span>Extra</span>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-book"></i>
+                                <span>Documentation</span>
+                                <span class="badge badge-pill badge-primary">Beta</span>
+                            </a>
+                        </li>
+                      
                     </ul>
                 </div>
+                <!-- sidebar-menu  -->
             </div>
-        </nav>
+            <!-- sidebar-content  -->
 
-        <main class="py-4">
+        </nav>
+        <!-- sidebar-wrapper  -->
+        <div class="app-bar">
+            <div class="d-flex justify-content-center">
+                <div class="col-md-8">
+                    <div class="d-flex justify-content-between">
+                        <a href="#" id="show-sidebar"><i class="fas fa-bars"></i></a>
+                        <h5 class="mb-0">@yield('title')</h5>
+                        <a href=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="py-4">
             @yield('content')
-        </main>
+        </div>
+        <div class="bottom-bar ">
+            <div class="d-flex justify-content-center ">
+                <div class="col-md-8">
+                    <div class="d-flex justify-content-between">
+                        <a href="">
+                            <i class="fa fa-home"></i>
+                            <p class='mb-0'>Home</p>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-home"></i>
+                            <p class='mb-0'>Home</p>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-home"></i>
+                            <p class='mb-0'>Home</p>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-home"></i>
+                            <p class='mb-0'>Home</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <!-- page-wrapper -->
 
     <!-- MDB -->
     <!-- JQuery -->
@@ -96,6 +158,46 @@
     </script>
     <!-- MDB core JavaScript -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+    <script>
+        jQuery(function($) {
+
+            $(".sidebar-dropdown > a").click(function() {
+                $(".sidebar-submenu").slideUp(200);
+                if (
+                    $(this)
+                    .parent()
+                    .hasClass("active")
+                ) {
+                    $(".sidebar-dropdown").removeClass("active");
+                    $(this)
+                        .parent()
+                        .removeClass("active");
+                } else {
+                    $(".sidebar-dropdown").removeClass("active");
+                    $(this)
+                        .next(".sidebar-submenu")
+                        .slideDown(200);
+                    $(this)
+                        .parent()
+                        .addClass("active");
+                }
+            });
+
+            $("#close-sidebar").click(function() {
+                $(".page-wrapper").removeClass("toggled");
+                $(".fa-bars").removeClass("invisible");
+            });
+            $("#show-sidebar").click(function(e) {
+                e.preventDefault();
+                $(".page-wrapper").addClass("toggled");
+                $(".fa-bars").addClass("invisible");
+            });
+
+
+
+
+        });
+    </script>
 </body>
 
 </html>
