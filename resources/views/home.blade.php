0eae0250943027ca6ec12201f9142de99b888bfa
fazza7777 <aungbarlaay7777@gmail.com>
2022-09-27 22:37:38 +0630

    employee end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
     <div class="card">
         <div class="card-body">
             <div class="row">
-                <div class="col-md-6">
-                    <div class="d-flex justify-content-start">
+                <div class="col-md-12">
+                    <div class="text-center">
                         <img src="{{ $employee->profile_image_path() }}" class="detail_profile_img" />
                         <div class="py-3 px-3">
-                            <h3>{{ $employee->name }}</h3>
+                            <h4>{{ $employee->name }}</h4>
                             <p class="mb-2 text-muted">{{ $employee->employee_id }}</p>
                             <p class="mb-2 text-muted"><span
                                     class="badge badge-pill badge-light border">{{ $employee->department ? $employee->department->title : '-' }}</span>
